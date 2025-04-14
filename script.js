8acbcda6e119ec774fc40f66700fb48a1772bb85
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-14 14:22:14 +0200

    created operator funtions add(), subtract(), multiply(), divide()
    tested with console.log()

---
+++ script.js
@@ -0,0 +1,20 @@
+//adds two numbers (integer or float -> return int or float)
+function add(operandLeft, operandRight) {
+    return (operandLeft + operandRight);
+}
+
+//subtracts a number from another number (int or float -> return int or float)
+function subtract(operandLeft, operandRight) {
+    return (operandLeft - operandRight);
+}
+
+//multiplies two numbers (int or float -> return int or float)
+function multiply(operandLeft, operandRight) {
+    return (operandLeft * operandRight);
+}
+
+//divides one number with another (int or float -> return int or float)
+function divide(operandLeft, operandRight) {
+    return (operandLeft / operandRight);
+}
+
